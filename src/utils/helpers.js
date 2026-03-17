f9c996158fcee60aa9d5517b9dd92a94d1b36e76
zxc19cxz <bboldchingis@gmail.com>
2026-03-17 20:42:38 +0000

    feat: add form validation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -57,3 +57,4 @@ module.exports = {
 // 2026-03-03 01:44:20 +08 - fix: fix race condition
 // 2026-03-03 13:50:14 +08 - fix: fix UI glitch
 // 2026-03-03 20:58:42 +08 - feat: add caching layer
+// 2026-03-18 04:42:38 +08 - feat: add form validation
