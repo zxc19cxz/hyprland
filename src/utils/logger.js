f4dbb26c81ad38b7212ef40336d8e768c6b192f0
zxc19cxz <bboldchingis@gmail.com>
2026-03-27 19:40:59 +0000

    fix: fix test failure and update related files

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -90,3 +90,4 @@ module.exports = {
 2026-03-26 21:52:26 +08 - related update
 2026-03-27 01:06:27 +08 - related update
 2026-03-27 22:11:07 +08 - related update
+2026-03-28 03:40:59 +08 - related update
